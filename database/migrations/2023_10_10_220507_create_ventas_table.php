7183f237127f703248ef8c08891d2aee65ee886c
Jesus <jesusmartines619@gmail.com>
2023-10-26 16:41:45 -0600

    Muchos a Muchos en proceso

--- database/migrations/2023_10_10_220507_create_ventas_table.php
+++ database/migrations/2023_10_10_220507_create_ventas_table.php
@@ -11,8 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('ventas', function (Blueprint $table) {
+        Schema::create('compras', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('producto_id')->constrained('productos');
+            $table->date('fecha');
+            $table->integer('cantidad');
             $table->timestamps();
         });
     }
@@ -22,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('ventas');
+        Schema::dropIfExists('compras');
     }
 };
